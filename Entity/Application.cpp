4a707a99ffc714f44480d5272b8098af61ddb4c7
sangjin149 <sangjin149@gmail.com>
2023-05-22 10:58:51 +0900

    feature: added appnum increase

--- Entity/Application.cpp
+++ Entity/Application.cpp
@@ -1,3 +1,5 @@
 #include "Entity.h"
 
-Application::Application(HireInfo* hireInfo) : appliedHireInfo(hireInfo) {}
+Application::Application(HireInfo* hireInfo) : appliedHireInfo(hireInfo) {
+  hireInfo->increaseApplicantNum();
+}
