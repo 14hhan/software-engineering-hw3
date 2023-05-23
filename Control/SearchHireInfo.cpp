1e1839cd21f3f91342b43fb1def38bd2b155e886
sangjin149 <sangjin149@gmail.com>
2023-05-24 00:44:39 +0900

    feat: SearchHireInfo의 생성자에 초기 데이터 설정 추가

--- Control/SearchHireInfo.cpp
+++ Control/SearchHireInfo.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 #include "./Control.h"
 
-SearchHireInfo::SearchHireInfo() {}
+SearchHireInfo::SearchHireInfo() {
+  this->hireInfoGroup = HireInfo::getAllHireInfo();
+  int size = hireInfoGroup.size();
+
+  hireList = new HireListItem[size];
+  for (int i = 0; i < size; i++) {
+    HireInfoSummary summary = hireInfoGroup[i]->getHireInfo();
+    HireListItem item;
+    item.groupIndex = i;
+    item.summary = summary;
+  }
+}
 
 vector<string> SearchHireInfo::sendSearchHireInfo() {}
+
+void SearchHireInfo::sortByCompanyName() {}
