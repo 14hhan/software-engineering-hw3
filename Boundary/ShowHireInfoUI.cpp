bc651afce1172721391a5d33687eaac18c26d00e
soomin <e5718@naver.com>
2023-05-24 18:49:59 +0900

    startinterface 추가

--- Boundary/ShowHireInfoUI.cpp
+++ Boundary/ShowHireInfoUI.cpp
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+#include <iostream>
 #include <string>
 
 #include "Boundary.h"
 
 // showHireInfo 컨트롤 클래스의 생성자 함수
-void showHireInfoUI::showHireInfo() {
+void showHireInfoUI::showHireInfo(showHireInfo* showHireInfo) {
   // 로그인 상태에서 회사 회원 자신이 등록한 채용 정보 조회 가능
   if (loginState) {
     // 클래스를 생성
     ShowHireInfoUI* ShowHireInfoUI = new ShowHireInfoUI;
   }
+};
+
+// showHireInfo UI를 보여주는 함수
+void ShowHireInfoUI::startInterface(){
+    //
 };
