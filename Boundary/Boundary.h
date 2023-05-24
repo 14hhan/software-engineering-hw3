1044e79b74b7ab1b2aaf7f1d9109e5c16e00e073
soomin <e5718@naver.com>
2023-05-24 19:05:43 +0900

    boundary.h 업데이트

--- Boundary/Boundary.h
+++ Boundary/Boundary.h
@@ -1,14 +1,21 @@
 #ifndef Boundary_h
 #define Boundary_h
 
-#include <Control.h>
-
 #include <string>
 
-class RegisterHireInfo {
+#include "Control.h"
+
+// 채용정보 등록 바운더리 클래스
+class RegisterHireInfoUI {
  public:
   void StartInterface();
   void sendHireInfoData(ShowHireInfo* showHireInfo);
+};
+
+// 채용정보 조회 바운더리 클래스
+class ShowHireInfoUI {
+ public:
+  void StartInterface();
 };
 
 #endif
