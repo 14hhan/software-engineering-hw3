da4aa09c9f0ed781d06cf5c487f5502fb0dcb663
soomin <e5718@naver.com>
2023-05-26 08:49:13 +0900

    채용정보조회 입출력 수정 및 주석정리

--- Boundary/ShowHireInfoUI.cpp
+++ Boundary/ShowHireInfoUI.cpp
@@ -1,10 +1,10 @@
-#include <stdio.h>
-
 #include <iostream>
-#include <string>
 #include <vector>
 
+using namespace std;
+
 #include "Boundary.h"
+#include "Control.h"
 
 ShowHireInfoUI::ShowHireInfoUI(ShowHireInfo* control) {
   this->control;
@@ -13,17 +13,17 @@ ShowHireInfoUI::ShowHireInfoUI(ShowHireInfo* control) {
 
 // showHireInfo UI를 보여주는 함수
 void ShowHireInfoUI::startInterface() {
-  cout << "3.2. 등록된 채용 정보 조회" << endl;
+  outputFile << "3.2. 등록된 채용 정보 조회";
 };
 
 void ShowHireInfoUI::showHireInfo() {
-  // ????
+  // 아래 코드 어떻게 수정해야할지?
   vector<HireInfo*> hireInfos = control->sendSearchHireInfo();
 
   // 출력
   // for 문 내에 어떻게 작성해야할지?
   for () {
-    cout << hireInfo->GetWorkType() << hireInfo->GetQuota()
-         << hireInfo->GetdueDate() << "endl";
+    outputFile << "> " << hireInfo->GetWorkType() << " " << hireInfo->GetQuota()
+               << " " << hireInfo->GetdueDate() << "\n\n";
   }
 }
