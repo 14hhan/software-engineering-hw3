f47d876ea3b42e28b128b8a42a4d1b31b7ef5265
soomin <e5718@naver.com>
2023-05-26 08:42:38 +0900

    채용정보등록 입출력변경

--- Boundary/RegisterHireInfoUI.cpp
+++ Boundary/RegisterHireInfoUI.cpp
@@ -4,22 +4,26 @@
 using namespace std;
 
 #include "Boundary.h"
+#include "Control/Control.h"
 
 // RegisterHireInfo UI를 보여주는 함수
-void RegisterHireInfoUI::StartInterface() {
-  cout << "3.1. 채용 정보 등록" << endl;
+void RegisterHireInfoUI::StartInterface(ofstream& outputFile) {
+  outputFile << "3.1. 채용 정보 등록";
 };
 
 // 입력받은 채용 정보들을 컨트롤 클래스에게 전달하는 함수
 // 값을 전달할 컨트롤 클래스:  RegisterHireInfo* registerHireInfo
-void RegisterHireInfoUI::InputHireInfoData(string workType, string dueDate,
-                                           int quota) {
+void RegisterHireInfoUI::InputHireInfoData(ifstream& inputFile,
+                                           ofstream& outputFile) {
+  string workType, dueDate;
+  int quota;
+
   // 업무, 인원수, 신청 마감일을 파일로 부터 입력받음
-  cin >> workType >> dueDate >> quota;
+  inputFile >> workType >> dueDate >> quota;
 
   // 입력받은 정보를 컨트롤 클래스로 전달
   control->sendHireInfoData(workType, dueDate, quota);
 
   // 출력
-  cout << workType, dueDate, quota << endl;
+  outputFile << "> " << workType << " " << dueDate << " " << quota << "\n\n";
 };
