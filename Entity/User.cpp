447011de5e618c2884e7b9655c70244edf3b45de
sangjin149 <sangjin149@gmail.com>
2023-05-26 04:02:28 +0900

    chore: User 주석 작성

--- Entity/User.cpp
+++ Entity/User.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #include "Entity.h"
 
 User::User(string id, int password) {
+  // id와 비밀번호를 입력받아 저장한다
   this->id = id;
   this->password = password;
 }
@@ -10,6 +11,7 @@ User::User(string id, int password) {
 NormalUser::NormalUser(string id, int password, string userName,
                        int registrationNumber)
     : User::User(id, password) {
+  // User의 상속자로 사용자 이름과 고유식별번호를 저장한다
   this->userName = userName;
   this->registrationNumber = registrationNumber;
 }
@@ -17,23 +19,27 @@ NormalUser::NormalUser(string id, int password, string userName,
 CompanyUser::CompanyUser(string id, int password, string companyName,
                          int companyNumber)
     : User::User(id, password) {
+  // User의 상속자로 회사명과 사업자 번호를 저장한다
   this->companyName = companyName;
   this->companyNumber = companyNumber;
 }
 
 string User::getUserInfo() {
+  // User 정보 요약이 담긴 문자열을 반환한다
   string summary = "";
   summary.append("ID:").append(id).append("\n");
   return summary;
 }
 
 string NormalUser::getUserInfo() {
+  // 위에 더해 일반 유저의 이름을 덧붙여 반환한다
   string summary = User::getUserInfo();
   summary.append("User Name:").append(userName).append("\n");
   return summary;
 }
 
 string CompanyUser::getUserInfo() {
+  // 위에 더해 회사 회원의 회사명을 덧붙여 반환한다
   string summary = User::getUserInfo();
   summary.append("Company Name:").append(companyName).append("\n");
   return summary;
