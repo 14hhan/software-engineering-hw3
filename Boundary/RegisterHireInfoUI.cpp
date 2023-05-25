f52b4018260370a11249e85adc588251a64ec572
soomin <e5718@naver.com>
2023-05-25 23:21:08 +0900

    추가

--- Boundary/RegisterHireInfoUI.cpp
+++ Boundary/RegisterHireInfoUI.cpp
@@ -6,14 +6,16 @@ using namespace std;
 #include "Boundary.h"
 
 // RegisterHireInfo UI를 보여주는 함수
-void RegisterHireInfoUI::StartInterface() { cout << "3.1. 채용 정보 등록"; };
+void RegisterHireInfoUI::StartInterface() {
+  cout << "3.1. 채용 정보 등록" << endl;
+};
 
 // 입력받은 채용 정보들을 컨트롤 클래스에게 전달하는 함수
 // 값을 전달할 컨트롤 클래스:  RegisterHireInfo* registerHireInfo
 void RegisterHireInfoUI::InputHireInfoData(string workType, string dueDate,
                                            int quota) {
   // 업무, 인원수, 신청 마감일을 파일로 부터 입력받음
-  // 구현 필요
+  cin >> workType >> dueDate >> quota;
 
   // 입력받은 정보를 컨트롤 클래스로 전달
   control->sendHireInfoData(workType, dueDate, quota);
