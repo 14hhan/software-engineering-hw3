4832fbec60ab7dba3f1bd30b917127c64671739d
didrl741 <didrl741@gmail.com>
2023-05-26 12:38:01 +0900

    [add] 출력 일부

--- Boundary/ShowApplianceInfoUI.cpp
+++ Boundary/ShowApplianceInfoUI.cpp
@@ -4,7 +4,13 @@ using namespace std;
 #include "Boundary.h"
 
 // 기능: 지원정보를 받아서 출력한다.
-void ShowApplianceInfoUI::startInterface(vector<HireInfoSummary> ApplicationOfNormalUser)
+void ShowApplianceInfoUI::startInterface(vector<HireInfoSummary> ApplicationOfNormalUser, ofstream& outputFile)
 {
+	// { [회사이름] [사업자번호] [업무] [인원 수] [신청마감일] }
+
+	for(int i=0; i<ApplicationOfNormalUser) {
+		outputFile << ApplicationOfNormalUser[i].companyName << ApplicationOfNormalUser[i].workType << ApplicationOfNormalUser[i].applicantNum << ApplicationOfNormalUser[i].dueDate << endl;
+	}
+
 	
 }
